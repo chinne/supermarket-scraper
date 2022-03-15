0e3f995e8f806ae9c4179a8669f052492d030c45
clinne <info@clinne.de>
2022-03-15 19:51:07 +0100

    black formatting

--- supermarkets/middlewares.py
+++ supermarkets/middlewares.py
@@ -53,7 +53,7 @@ class SupermarketsSpiderMiddleware:
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class SupermarketsDownloaderMiddleware:
@@ -100,4 +100,4 @@ class SupermarketsDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
