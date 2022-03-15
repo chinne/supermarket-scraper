dfcf4c541aa4ff1ca72688319caebfc31f1b349f
clinne <info@clinne.de>
2022-03-15 19:49:33 +0100

    Initial commit

---
+++ supermarkets/spiders/aldi.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+import scrapy
+
+
+class AldiSpider(scrapy.Spider):
+    name = 'aldi'
+    allowed_domains = ['www.aldi-nord.de']
+    start_urls = ['https://www.aldi-nord.de/angebote.html']
+
+    def parse(self, response):
+        """a method that will be called to handle the response downloaded for each of the requests made. The response parameter is an instance 
+        of TextResponse that holds the page content and has further helpful methods to handle it.
+        The parse() method usually parses the response, extracting the scraped data as dicts and also finding new URLs to follow and creating new requests (Request) from them."""
+        # page = response.url.split("/")[-2]
+        # articles = response.xpath('//*[@class="mod-article-tile__content"]')
+        print('responsen', response.text)
+        article = response.xpath('//*[@class="mod-article-tile__title"]/text()').extract()
+        price = response.xpath('//*[@class="price__main"]/text()').extract()
+        description_short = response.xpath('//*[@class="mod-article-tile__info"]/p/text()').extract()
+        # print(articles)
+        price_list = price[0::2]
+        product_list = article[1::2]
+        for i, _ in enumerate(price_list):
+            # print(articl)
+            # print(price)
+            product = article.xpath('//*[@class="mod-article-tile__title"]/text()').extract(),
+            price = article.xpath('//*[@class="price__main"]/text()').extract()
+            print(product_list[i].strip())
+            yield {
+                'Produkt': product_list[i].strip(), 'Beschreibung': description_short[i].strip(), 'Preis': price_list[i].strip()
+            }
+        
---
+++ supermarkets/spiders/goasia.py
@@ -0,0 +1,30 @@
+import scrapy
+
+
+class GoasiaSpider(scrapy.Spider):
+    name = 'goasia'
+    allowed_domain = ['https://goasia.net/onlineshop']
+    start_urls = ['https://goasia.net/onlineshop/?p=1']
+
+
+    # def start_requests(self)
+    #     yield scrapy.Request(f'https://goasia.net/onlineshop/?p={self.page}')
+
+    def parse(self, response):
+        for page in range(1, 120):
+            for products in response.css('div.product--info'):
+                if products.css('span.buy-btn--cart-text::text').get():
+                    available = 'yes'
+                else:
+                    available = 'sold out'
+                yield {
+                    'name': products.css('a.product--title').attrib['title'],
+                    'price': products.css('span.price--default.is--nowrap::text').get().split()[0],
+                    'available': available,
+                    'link': products.css('a.product--title').attrib['href']
+                }
+            
+            next_page_url = f'https://goasia.net/onlineshop/?p={str(page)}'
+            if next_page_url:
+                request = scrapy.Request(url=next_page_url)
+                yield request
